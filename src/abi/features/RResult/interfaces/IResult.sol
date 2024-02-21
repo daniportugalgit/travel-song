00561b85490c1b8860f2ec98d2be22a2b6a7e266
Daniel Portugal <daniportugal@gmail.com>
2024-02-21 12:39:45 -0300

    tpush

--- src/abi/features/RResult/interfaces/IResult.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-interface IResult {
-  function get(address a, uint256 b, uint256 c, uint256 d, uint256 e) external view returns (uint256);
-}
